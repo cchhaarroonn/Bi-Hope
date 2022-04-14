ffbc7c882da4531e5e0fc94ca9ec75e33889ae81
charon <90192366+cchhaarroonn@users.noreply.github.com>
2022-04-14 14:09:42 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+import pymem
+import pymem.process
+import keyboard
+import time
+from colorama import Fore
+
+dwLocalPlayer = 0xDB65DC
+dwForceJump = 0x527C38C
+m_fFlags = 0x104
+RESET = Fore.RESET
+BLUE = Fore.BLUE
+
+print(f"""{BLUE}
+
+$$$$$$$\  $$\         $$\   $$\                               
+$$  __$$\ \__|        $$ |  $$ |                              
+$$ |  $$ |$$\         $$ |  $$ | $$$$$$\   $$$$$$\   $$$$$$\  
+$$$$$$$\ |$$ |$$$$$$\ $$$$$$$$ |$$  __$$\ $$  __$$\ $$  __$$\ 
+$$  __$$\ $$ |\______|$$  __$$ |$$ /  $$ |$$ /  $$ |$$$$$$$$ |
+$$ |  $$ |$$ |        $$ |  $$ |$$ |  $$ |$$ |  $$ |$$   ____|
+$$$$$$$  |$$ |        $$ |  $$ |\$$$$$$  |$$$$$$$  |\$$$$$$$\ 
+\_______/ \__|        \__|  \__| \______/ $$  ____/  \_______|
+                                          $$ |                
+                                          $$ |                
+                                          \__|                          
+{RESET}
+""")
+
+hotkey = str(input("Hotkey: "))
+
+
+pm = pymem.Pymem("csgo.exe")
+client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+while True:
+    if keyboard.is_pressed("escape"):
+        exit()
+
+    if keyboard.is_pressed(hotkey):
+        force_jump = client+dwForceJump
+        force_left = client+dwForceLeft
+        force_right = client+dwForceRight
+        player = pm.read_int(client+dwLocalPlayer)
+        onGround = pm.read_int(player+m_fFlags)
+        if player and onGround and onGround == 257:
+            pm.write_int(force_jump, 6)
+            time.sleep(0.08)
+            pm.write_int(force_jump, 6)
